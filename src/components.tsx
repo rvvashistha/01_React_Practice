47fd6589876f8c2d804778c7e8768ecc837c1a22
rvvashistha <rohitvashistha171@gmail.com>
2023-08-13 16:13:03 +0530

    first commit

---
+++ src/components.tsx
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react'
+
+
+const Phara = () => {
+    const el = (<div title='Welcome to React'>
+        <h1>An Introduction to React</h1>
+        <p id='introduction'>
+            <span className='"test-bold'>React</span>
+            &nbsp; is a JS library for building the UI lets learn it
+        </p>
+
+    </div>)
+    return el
+}
+
+
+
+const new_el = (
+    <Fragment>
+        <Phara></Phara>
+        <Phara></Phara>
+    </Fragment>
+)
+
+
+
+export { new_el } 
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {new_el,} from './components';
+
+ReactDOM.render(new_el, document.getElementById('root'))
