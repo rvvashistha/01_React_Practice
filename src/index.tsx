8d5abf4cdeb512e053859eb779ccb667c59ac9c5
rvvashistha <rohitvashistha171@gmail.com>
2023-08-13 16:19:38 +0530

    second commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {new_el,} from './components';
 
-ReactDOM.render(new_el, document.getElementById('root'))
+ReactDOM.render(new_el, document.getElementById('root')) //!!
